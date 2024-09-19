4c5dec70cc204704bb5bcbc47f0ccc9f000f19df
Sanjay Kumar <sanjaykumarmohanta465@gmai.com>
2024-09-20 00:47:51 +0530

    DSA

--- DSA/AnytimePractice.java
+++ DSA/AnytimePractice.java
@@ -23,12 +23,48 @@ public class AnytimePractice {
             System.out.println("maximum sum is :" + maxsum);   
 
         } 
+public static void kadanes(int numbers[]) {
+    int ms = Integer.MIN_VALUE;
+    int cs = 0;
+    for(int i = 0; i<numbers.length; i++)  {
+        cs = cs + numbers[i];
+
+        if(cs<0) {
+            cs = 0; 
+        }
+        ms = Math.max(cs,ms);
+    }
+    System.out.println("The maximum sum is :" + ms);
+}
+
+//RAIN WATER TRAPPED
+
+public static int RainwaterTrapped(int height[])  {
+    int leftmax[] = new int[height.length];
+    leftmax[0] = height[0];
+    for(int i = 1; i<height.length; i++)  {
+        leftmax[i] = Math.max(height[i],leftmax[i-1]);
+    } 
+    int rightmax[] = new int[height.length];
+    rightmax[height.length-1] = height[height.length-1];
+    for(int i = (height.length - 2); i>=0;i--)  {
+              rightmax[i] = Math.max(height[i] ,rightmax[i+1]);
+    }
+    int trappedwater = 0;
+    for(int i = 0; i<height.length;i++)  {
+        int waterlevel =Math.min(leftmax[i] , rightmax[i]);
+        trappedwater += waterlevel - height[i];
+    }
+    return trappedwater;
+}
+
      
         
 
     public static void main(String[] args) {
-        int numbers[] = {2 , 4 , 6 , 8 , 10 };
-        Maxsum(numbers);
+        int height[] = {4 , 2 , 0 , 6 , 3 , 2 , 5};
+        System.out.println(" The amount of water trapped :" + RainwaterTrapped(height));
+       
 
     }
 
